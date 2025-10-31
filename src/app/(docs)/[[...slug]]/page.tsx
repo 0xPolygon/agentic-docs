9868297a8a3e79f507978f5ad3ec0d0e5dd5dc94
cooganb <coogan.brennan@gmail.com>
2025-10-31 16:32:39 -0400

    updates layout

--- src/app/(docs)/[[...slug]]/page.tsx
+++ src/app/(docs)/[[...slug]]/page.tsx
@@ -21,8 +21,10 @@ export default async function Page(props: PageProps<'/[[...slug]]'>) {
 
   const MDX = page.data.body;
 
+  const isIndexPage = slug.join('/') === 'general';
+
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage toc={isIndexPage ? [] : page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
